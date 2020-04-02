eaae20490a45cd0c1d42cb4e950a3c8ae83d7daa
RaniAgus <39303639+RaniAgus@users.noreply.github.com>
2020-04-02 15:46:35 -0300

    Agregadas dos funciones para ultima letra

--- src/CerebroMagico.hs
+++ src/CerebroMagico.hs
@@ -9,5 +9,11 @@ esVocal letra = elem letra vocales
 esParticular :: String -> Bool
 esParticular nombre = esVocal(head nombre) || length nombre > 7
 
+ultimaLetra :: String -> Char
+ultimaLetra palabra = palabra !! (length palabra - 1)
+
+esVocalUltimaLetra :: String -> Bool
+esVocalUltimaLetra = esVocal.ultimaLetra
+
 esMagico :: String -> Bool
-esMagico nombre = esParticular nombre && not (esVocal (nombre !! (length nombre - 1)))
+esMagico nombre = esParticular nombre && not (esVocalUltimaLetra nombre)
