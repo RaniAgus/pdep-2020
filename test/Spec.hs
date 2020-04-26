3c38018db2467b28631fa17283a2fa66fad2ff47
“Agustin <aguseranieri@gmail.com>
2020-04-25 21:50:58 -0300

    descripcion mejorada de los tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -13,12 +13,12 @@ main = hspec $ do
         esParticular "Ana" `shouldBe` True
      it "nombre largo es particular" $ do
         esParticular "Fernando" `shouldBe` True
-     it "nombre no es particular" $ do
+     it "nombre corto que comienza con consonante no es particular" $ do
         esParticular "Beto" `shouldBe` False
   describe "esMagico" $ do
      it "nombre que termina en vocal no es magico" $ do
         esMagico "Ana" `shouldBe` False
      it "nombre que no es particular no es magico" $ do
         esMagico "Flor" `shouldBe` False
-     it "nombre es magico" $ do
+     it "nombre que es particular y termina en consonante es magico" $ do
         esMagico "Omar" `shouldBe` True
