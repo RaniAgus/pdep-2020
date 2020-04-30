7470e1892c916569d964f123d8ae6bbdce1f2675
“Agustin <aguseranieri@gmail.com>
2020-04-30 11:55:56 -0300

    lib.hs

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,40 @@
 module Lib where
 
 laVerdad = True
+
+type Precio = Double
+type Nombre = String
+type Color = String
+type CriterioPropina = Precio -> Precio
+
+cuantoPagar :: CriterioPropina -> Precio -> Precio
+cuantoPagar criterioPropina cuenta = cuenta + criterioPropina cuenta
+
+propinaRecomendada :: CriterioPropina
+propinaRecomendada cuenta = cuenta * 0.1
+
+noCreeEnPropina :: CriterioPropina
+noCreeEnPropina _ = 0
+
+propinaConservadora :: CriterioPropina
+propinaConservadora cuenta 
+    | esPrecioPar cuenta = 20
+    | otherwise = 25
+
+esPrecioPar :: Precio -> Bool
+esPrecioPar = even.floor
+
+propinaTioCarlos :: Nombre -> CriterioPropina
+propinaTioCarlos "Carlos" cuenta  = ((*2).propinaRecomendada) cuenta
+propinaTioCarlos _ cuenta = ((/2).propinaRecomendada) cuenta
+
+propinaPayasa :: Color -> CriterioPropina
+propinaPayasa "Azul" cuenta  = propinaRecomendada cuenta
+propinaPayasa "Roja" _ = 100
+propinaPayasa "Negra" _  = 0
+
+propinaSegunCosto :: CriterioPropina
+propinaSegunCosto cuenta
+    | cuenta < 500 = ((*3).propinaRecomendada) cuenta
+    | cuenta < 1000 = propinaConservadora cuenta
+    | otherwise = 0
