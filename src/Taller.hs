e396664e04f1feb8cfeb482d50e54c92b64e686c
“Agustin <aguseranieri@gmail.com>
2020-05-10 17:45:57 -0300

    hago refactor punto 1 y puntos del integrante a

--- src/Taller.hs
+++ src/Taller.hs
@@ -3,49 +3,81 @@ module Taller where
 type Desgaste = Float
 type Patente = String
 type Fecha = (Int, Int, Int)
+type Costo = Int
  
 -- Definiciones base
 anio :: Fecha -> Int
 anio (_, _, year) = year
  
 data Auto = Auto {
- patente :: Patente,
- desgasteLlantas :: [Desgaste],
- rpm :: Float,
- temperaturaAgua :: Float,
- ultimoArreglo :: Fecha
+    patente :: Patente,
+    desgasteLlantas :: [Desgaste],
+    rpm :: Float,
+    temperaturaAgua :: Float,
+    ultimoArreglo :: Fecha
 } deriving Show
-ferrari =Auto{
- patente ="00aa0a0",
- desgasteLlantas = [1.0, 0.1, 0, 0.2],
- rpm = 0.5,
- temperaturaAgua = 1.5,
- ultimoArreglo = (17,11,1999)
+
+unAuto = Auto {
+    patente ="AAA000",
+    desgasteLlantas = [1.0, 0.1, 0, 0.2],
+    rpm = 3000,
+    temperaturaAgua = 1.5,
+    ultimoArreglo = (17,11,1999)
 } 
 
-calculoPatental ::String ->Int
-calculoPatental  patente 
-    |last patente == '4' = ((*3000).length) patente   
-    |otherwise =20000
-
-costoDeReparacion ::Auto ->Int
+-- Punto 1
+costoDeReparacion :: Auto -> Costo
 costoDeReparacion auto
-    |(length.patente) auto == 7 =12500
-    |(length.patente) auto == 7 && (init.patente) auto == "DJ" && (tail.patente) auto ==  "NB" = (calculoPatental.patente) auto
-    |otherwise =15000
---parte 2
-autoPeligroso:: Auto->Bool
-autoPeligroso auto = ((>0.5).(head.desgasteLlantas)) auto
+    | esPatenteNueva auto = 12500
+    | esPatenteAnterior auto && (patenteEstaEntre "DJ" "NB") auto = (calculoPatental.patente) auto
+    | otherwise = 15000
 
-año::Fecha->Int
+esPatenteNueva :: Auto -> Bool
+esPatenteNueva = (==7).length.patente
+
+esPatenteAnterior :: Auto -> Bool
+esPatenteAnterior = (==6).length.patente
+
+patenteEstaEntre :: Patente -> Patente -> Auto -> Bool
+patenteEstaEntre patente1 patente2 auto = patente1 < (patente auto) && (patente auto) < patente2
+
+calculoPatental :: Patente -> Costo
+calculoPatental  patente 
+    | last patente == '4' = 3000 * length patente
+    | otherwise = 20000
+
+-- Punto 2
+-- Parte 1 (integrante a)
+esAutoPeligroso :: Auto -> Bool
+esAutoPeligroso = estaMuyDesgastada.primeraLlanta
+
+primeraLlanta :: Auto -> Desgaste
+primeraLlanta = head.desgasteLlantas
+
+estaMuyDesgastada :: Desgaste -> Bool
+estaMuyDesgastada = (>0.5)
+
+-- Parte 2 (integrante b)
+año:: Fecha -> Int
 año (_,_,año) = año
 
 necesitaRevision ::Auto ->Bool
 necesitaRevision auto = ((<2015).(año.ultimoArreglo)) auto 
 
---parte3
+-- Punto 3
+-- Parte 1 (integrante a)
+alfa :: Auto -> Auto
+alfa unAuto 
+    | rpm unAuto < 2000 = unAuto
+    | otherwise = unAuto { rpm = 2000 }
 
+bravo :: Auto -> Auto
+bravo unAuto = unAuto { desgasteLlantas = sinDesgaste }
 
+sinDesgaste :: [Desgaste]
+sinDesgaste = [0, 0, 0, 0]
 
+charly :: Auto -> Auto
+charly = bravo.alfa
 
-
+-- Parte 2 (integrante b)
