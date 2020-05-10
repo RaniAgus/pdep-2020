5217a082c8e96784649827f74b01cbcd55a39d2f
Erik <erikquispechoque@gmail.com>
2020-05-10 15:52:55 -0300

    parte 1 y 2

--- src/Taller.hs
+++ src/Taller.hs
@@ -15,3 +15,37 @@ data Auto = Auto {
  temperaturaAgua :: Float,
  ultimoArreglo :: Fecha
 } deriving Show
+ferrari =Auto{
+ patente ="00aa0a0",
+ desgasteLlantas = [1.0, 0.1, 0, 0.2],
+ rpm = 0.5,
+ temperaturaAgua = 1.5,
+ ultimoArreglo = (17,11,1999)
+} 
+
+calculoPatental ::String ->Int
+calculoPatental  patente 
+    |last patente == '4' = ((*3000).length) patente   
+    |otherwise =20000
+
+costoDeReparacion ::Auto ->Int
+costoDeReparacion auto
+    |(length.patente) auto == 7 =12500
+    |(length.patente) auto == 7 && (init.patente) auto == "DJ" && (tail.patente) auto ==  "NB" = (calculoPatental.patente) auto
+    |otherwise =15000
+--parte 2
+autoPeligroso:: Auto->Bool
+autoPeligroso auto = ((>0.5).(head.desgasteLlantas)) auto
+
+año::Fecha->Int
+año (_,_,año) = año
+
+necesitaRevision ::Auto ->Bool
+necesitaRevision auto = ((<2015).(año.ultimoArreglo)) auto 
+
+--parte3
+
+
+
+
+
