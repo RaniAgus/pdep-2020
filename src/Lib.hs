2c04e9d7f9f42d30c41bcab0ed6d44790e0024bb
“Agustin <aguseranieri@gmail.com>
2020-05-28 15:43:51 -0300

    primer intento de resolución

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,13 +6,13 @@ module Lib where
 type Poder = Float
 type Durabilidad = Int
 
-data ParteDeArmadura = ParteDeArmadura {
+data Parte = Parte {
     defensa :: Poder,
     durabilidad :: Durabilidad
 } deriving Show
 
 --Ataque
-data Material = Madera | Metal deriving (Show,Eq)
+data Material = Madera | Metal | Otro deriving (Show,Eq)
 
 type Ataque = Float
 data Arma = Baculo { nombre:: String, inteligencia:: Poder} 
@@ -23,56 +23,154 @@ data Arma = Baculo { nombre:: String, inteligencia:: Poder}
 data Personaje = Personaje {
     arma :: Arma,
     vida :: Poder,
-    armadura :: [ParteDeArmadura]
+    armadura :: [Parte]
 } deriving Show
 
---Punto 1
-armadurasNoRotas :: Personaje -> [ParteDeArmadura]
-armadurasNoRotas unPersonaje = filter ((>0).durabilidad) (armadura unPersonaje)
+--Buffs
+type Buff = Personaje -> Personaje
 
-calcularDefensa :: [ParteDeArmadura] -> Poder
-calcularDefensa = (sum.map defensa)
-
+--Parte 1: Defensa
 poderDeDefensa :: Personaje -> Poder
-poderDeDefensa unPersonaje = vida unPersonaje + (calcularDefensa.armadurasNoRotas) unPersonaje
+poderDeDefensa personaje = vida personaje + defensaTotal personaje
 
---Punto 2
+defensaTotal :: Personaje -> Poder
+defensaTotal = calcularDefensa.armadurasNoRotas
+
+calcularDefensa :: [Parte] -> Poder
+calcularDefensa = sum.map defensa
+
+armadurasNoRotas :: Personaje -> [Parte]
+armadurasNoRotas = filter (not.estaRota).armadura 
+
+estaRota :: Parte -> Bool
+estaRota = (==0).durabilidad
+
+--Parte 2: Ataque
 poderDeAtaque :: Personaje -> Poder
 poderDeAtaque = poderDeArma.arma
 
 poderDeArma :: Arma -> Poder
-poderDeArma (Baculo nom intelig) = intelig + (fromIntegral.length) nom
-poderDeArma (Arco rang long danio) = rang * long + danio
-poderDeArma (Espada alm mat) = alm * (coeficienteDeMaterial mat)
+poderDeArma (Baculo nombre inteligencia) = inteligencia + (fromIntegral.length) nombre
+poderDeArma (Arco rango longitud daño) = rango * longitud + daño
+poderDeArma (Espada almas material) = almas * (coeficienteDeMaterial material)
 
 coeficienteDeMaterial :: Material -> Poder
 coeficienteDeMaterial Madera = 2
 coeficienteDeMaterial Metal = 3
 coeficienteDeMaterial _ = 1
 
---Punto 3
-type Buff = Personaje -> Personaje
+--Parte 3: Buffs
+--Funciones "transformar" a nivel Personaje
+transformarArma :: (Arma->Arma)-> Buff
+transformarArma f personaje = personaje {
+    arma = (f.arma) personaje
+}
 
+transformarVida f personaje = personaje {
+    vida = (f.vida) personaje
+}
+
+transformarArmadura :: (Parte->Parte) -> Buff
+transformarArmadura f personaje = personaje {
+    armadura = (map f.armadura) personaje
+}
+
+--Funciones "transformar" a nivel Parte
+transformarDefensa :: (Poder->Poder) -> Parte -> Parte
+transformarDefensa f parte = parte {
+    defensa = (f.defensa) parte
+}
+
+transformarDurabilidad :: (Durabilidad->Durabilidad) -> Parte -> Parte
+transformarDurabilidad f parte = parte {
+    durabilidad = (f.durabilidad) parte
+}
+
+--Frenesí
 frenesi :: Buff
-frenesi unPersonaje = unPersonaje { armadura = buffDefensaArmadura (*1.20) unPersonaje }
+frenesi = transformarArmadura (transformarDefensa (*1.20))
 
-buffDefensaArmadura :: (Poder->Poder) -> Personaje -> [ParteDeArmadura]
-buffDefensaArmadura buff unPersonaje = map (\parte -> parte{defensa = (buff.defensa) parte}) (armadura unPersonaje)
-
+--Manto Etéreo
 mantoEtereo :: Buff
-mantoEtereo unPersonaje = unPersonaje { vida = (quitarVida 100) unPersonaje, armadura = buffDefensaArmadura (+3) unPersonaje }
+mantoEtereo = transformarVida (quitarVida 100).transformarArmadura (transformarDefensa (+3))
 
-quitarVida :: Poder -> Personaje -> Poder
-quitarVida valor unPersonaje = vida unPersonaje - valor
+quitarVida :: Poder -> Poder -> Poder
+quitarVida cantidad vida = vida - cantidad
 
+--Berserker
 berserker :: Buff
-berserker unPersonaje = unPersonaje { armadura = buffDefensaArmadura (\x->2) unPersonaje, arma = (transformarEnMetal.arma) unPersonaje }
+berserker = transformarArmadura (transformarDefensa (\x->2)).transformarArma metalizar
 
-transformarEnMetal :: Arma -> Arma
-transformarEnMetal (Espada almas Madera) = Espada almas Metal  
-transformarEnMetal unArma@(Espada _ _) = id unArma 
-transformarEnMetal unArma@(Baculo _ _) = id unArma 
-transformarEnMetal unArma@(Arco _ _ _) = id unArma 
+metalizar :: Arma -> Arma
+metalizar (Espada almas Madera) = Espada almas Metal  
+metalizar arma = arma 
 
+--Espejo de Karma
 espejoDeKarma :: Buff -> Buff
 espejoDeKarma buff = buff.buff
+
+--buffCreativo :: Buff -> Buff
+repararArmadura :: Buff
+repararArmadura = transformarArmadura (transformarDurabilidad (\x->10))
+
+--Sucesion de buffs
+potenciarMultiple :: Personaje -> [Buff] -> Personaje
+potenciarMultiple personaje buffs = foldl potenciar personaje buffs
+
+--Potenciar
+potenciar :: Personaje -> Buff -> Personaje 
+potenciar personaje buff = buff personaje
+
+--Es inofensivo
+esInofensivo :: Buff -> [Personaje] -> Bool
+esInofensivo buff = not.any (cambiaronStats buff)
+
+cambiaronStats :: Buff -> Personaje -> Bool
+cambiaronStats buff personaje = not (
+    diferenciaPoder poderDeAtaque personaje (buff personaje) == 0 &&
+    diferenciaPoder poderDeDefensa personaje (buff personaje) == 0 )
+
+diferenciaPoder :: (Personaje->Poder) -> Personaje -> Personaje -> Poder
+diferenciaPoder f personaje1 personaje2 = f personaje1 - f personaje2
+
+--Parte 4: Desgaste
+desgastar :: Durabilidad -> Personaje -> Personaje
+desgastar intensidad personaje = personaje {
+    armadura = desgastarArmadura intensidad (armadura personaje)
+}
+
+desgastarArmadura :: Durabilidad -> [Parte] -> [Parte]
+desgastarArmadura _ [] = []
+desgastarArmadura intensidad (x:xs) = desgastarParte intensidad x : 
+    desgastarArmadura (intensidad `div` 2) xs
+
+desgastarParte :: Durabilidad -> Parte -> Parte
+desgastarParte intensidad parte = parte {
+    durabilidad = max (durabilidad parte - intensidad) 0
+}
+
+--Parte 5: Clanes
+data Clan = Clan {
+    miembros :: [Personaje],
+    buffs :: [Buff]
+}
+
+leGana :: Clan -> Clan -> Bool
+leGana atacante defensor = poderTotalSegun poderDeAtaque atacante > poderTotalSegun poderDeDefensa defensor
+
+poderTotalSegun :: (Personaje -> Poder) -> Clan -> Poder
+poderTotalSegun f = ( sum . map f . potenciarClanSegun f )
+
+potenciarClanSegun :: (Personaje -> Poder) -> Clan -> [Personaje]
+potenciarClanSegun f clan = ( map ( potenciarMiembroSegun f (buffs clan) ) . miembros ) clan
+
+potenciarMiembroSegun :: (Personaje -> Poder) -> [Buff] -> Buff
+potenciarMiembroSegun f buffs miembro = potenciar miembro (elegirMejorBuff f buffs miembro)
+
+elegirMejorBuff :: (Personaje -> Poder) -> [Buff] -> Personaje -> Buff
+elegirMejorBuff f buffs miembro = foldl1 (esMejorSegun (f.potenciar miembro)) buffs
+
+esMejorSegun :: (Buff -> Poder) -> Buff -> Buff -> Buff
+esMejorSegun g buff1 buff2
+    | g buff1 > g buff2 = buff1 
+    | otherwise = buff2
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,35 @@
 import Test.Hspec
 import Lib
 
+unaEspada = Espada {
+   almas = 3,
+   material = Metal
+}
+
+unArco = Arco {
+   rango = 2,
+   longitudHilo = 3,
+   dañoBase = 5
+}
+
+unBaculo = Baculo {
+   nombre = "unbaculo",
+   inteligencia = 5
+}
+
+unArmadura = [Parte{defensa = 1, durabilidad = 10},
+    Parte{defensa= 2, durabilidad = 10},
+    Parte{defensa= 3, durabilidad = 0},
+    Parte{defensa= 4, durabilidad = 10}]
+
+unPersonaje = Personaje {
+   arma = unaEspada,
+   vida = 100,
+   armadura = unArmadura
+}
+
 main :: IO()
 main = hspec $ do
    describe "Tests de prueba" $ do
       it "la verdad y la verdad" $ do
-         laVerdad `shouldBe` True
+         True `shouldBe` True
